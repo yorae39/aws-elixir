6f722de229f565f7c3eeab2141da000d86f5d6a3
Luiz Paulo <lpaureliano74@gmail.com>
2019-07-13 20:21:00 -0300

    Criando o projeto

---
+++ hello_world/.elixir_ls/build/test/lib/hello_world/ebin/hello_world.app
@@ -0,0 +1,7 @@
+{application,hello_world,
+             [{applications,[kernel,stdlib,elixir,logger,
+                             aws_lambda_elixir_runtime,distillery]},
+              {description,"hello_world"},
+              {modules,['Elixir.HelloWorld']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
---
+++ hello_world/mix.exs
@@ -0,0 +1,28 @@
+defmodule HelloWorld.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :hello_world,
+      version: "0.1.0",
+      elixir: "~> 1.8",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:aws_lambda_elixir_runtime, "~> 0.1.0"},
+      {:distillery, "~> 2.0"}
+    ]
+  end
+end
